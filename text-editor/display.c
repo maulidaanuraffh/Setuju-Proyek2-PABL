638299982c0f1a2f5de88d741ed8463b3cdb7b75
AzIzzyy17 <ikhwan1190.syahid@gmail.com>
2026-03-28 01:21:41 +0700

    feat(display): Menambahkan fungsi flush_stdin dan fungsi baca_baris_aman

--- text-editor/display.c
+++ text-editor/display.c
@@ -9,12 +9,33 @@
 #include "editor.h"
 #include "display.h"
 
-/* clear_screen()*/
-
+/*   clear_screen()   */
 void clear_screen(void){
 #ifdef _WIN32
     system("cls");
 #else 
-    system("clear")
+    system("clear");
 #endif
+}
+
+/*   flush_stdin()   */
+void flush_stdin(void){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
+
+/*   baca_baris_aman()   */
+int baca_baris_aman(char *buf, int max ){
+    if (buf == NULL || max <=0 ) return -1;
+    if (fgets (buf, max, stdin) == NULL){
+        buf[0] = '\0';
+        return -1;
+    }
+
+    /* FIX: strip \r\n (Windows & Unix) */
+    buf[strcspn(buf, "\r\n")] = '\0';
+
+    /* deteksi overflow: ada sisa di stdin */
+    if ((int)strlen(buf) == max - 1) flush_stdin();
 }
