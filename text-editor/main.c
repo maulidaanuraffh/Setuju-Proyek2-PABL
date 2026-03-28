52d8336f1c8848e4d702dbe861a8f230b0b81d7f
maulidaanuraffh <maulidaaa.nurafifah@gmail.com>
2026-03-28 15:23:53 +0700

    test(main): delete_baris logic

--- text-editor/main.c
+++ text-editor/main.c
@@ -6,9 +6,10 @@ int main() {
 
     // test init_editor 
     init_editor(&ed); 
-    printf("init OK: jumlah_baris=%d\n", ed.jumlah_baris);
+    printf("init OK: jumlah_baris=%d\n\n", ed.jumlah_baris);
 
     // test insert_baris
+    printf("----TEST INSERT----\n");
     insert_baris(&ed, 0, "proyek 2");
     insert_baris(&ed, 1, "struktur data");
     printf("insert OK: jumlah_baris=%d\n", ed.jumlah_baris);
@@ -20,6 +21,14 @@ int main() {
     printf("baris 0: %s\n", ed.buffer[0]); // bakal ttp "proyek 2"
     printf("baris 1: %s\n", ed.buffer[1]); // hrsnya jd "sisipan tengah"
     printf("baris 2: %s\n", ed.buffer[2]); // jd "struktur data" 
+
+    // test delete_baris
+    printf("----TEST DELETE----\n");
+    delete_baris(&ed, 0);
+    printf("delete OK: jumlah_baris=%d\n", ed.jumlah_baris);
+    printf("Setelah delete indeks 0:\n");
+    printf("baris 0: %s\n", ed.buffer[0]); // skrg jadi "sisipan tengah"
+    printf("baris 1: %s\n", ed.buffer[1]); // "struktur data" 
 
     bebaskan_buffer(&ed);
     printf("bebaskan OK\n");
