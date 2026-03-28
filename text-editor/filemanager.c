062001f3d2cdf5243cf5744421fb8bc7780907c2
AzIzzyy17 <ikhwan1190.syahid@gmail.com>
2026-03-29 00:24:10 +0700

    feat(filemanager):menambah fungsi ambil_nama_file

---
+++ text-editor/filemanager.c
@@ -0,0 +1,30 @@
+/* ============================================================
+ * filemanager.c 
+ * Proyek 2 |  Ikhwan Syahid Azizy (013)
+ * ============================================================ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "editor.h"
+#include "filemanager.h"
+#include "display.h"
+
+/*   ambil_nama_file   */
+
+char *ambil_nama_file(const char *path){
+    char *s1, *s2, *terakhir;
+
+    if(path == NULL) return NULL;
+
+    s1=strrchr(path, '/');
+    s2=strrchr(path, '\\');
+
+    
+    if (s1 == NULL && s2 == NULL) return (char *)path;
+    if (s1 == NULL)  terakhir = s2;
+    else if (s2 == NULL) terakhir = s1;
+    else terakhir = (s1 > s2) ? s1 : s2;
+
+    return terakhir + 1;
+}
