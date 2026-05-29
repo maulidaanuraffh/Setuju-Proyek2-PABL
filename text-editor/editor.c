3c544e1547818d2c38140846b85057728ead5ba9
AzIzzyy17 <ikhwan1190.syahid@gmail.com>
2026-05-29 08:54:52 +0700

    feat(editor):delete + go to line + word count

--- text-editor/editor.c
+++ text-editor/editor.c
@@ -1,12 +1,13 @@
 /* ============================================================
    editor.c 
-   Proyek 2 |  Maulida Nur Afifah  (017)
+   Proyek 2 |  Maulida Nur Afifah (017) & Ikhwan Syahid Azizy (013)
  * ============================================================ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "editor.h"
 
+// Maulida Nur Afifah (017)
 // buat satu RowNode baru dengan isi string yang disalin
 RowNode *buat_row_node(const char *teks) {
     RowNode *baris;
@@ -36,6 +37,7 @@ RowNode *buat_row_node(const char *teks) {
     return baris;
 }
 
+// Ikhwan Syahid Azizy (013)
 // bebaskan satu rantai RowNode mulai dari head
 void bebaskan_row_list(RowNode *head) {
     RowNode *sekarang  = head;
@@ -49,6 +51,7 @@ void bebaskan_row_list(RowNode *head) {
     }
 }
 
+// Ikhwan Syahid Azizy (013)
 // bebaskan semua baris di editor dan pointer
 void bebaskan_semua_baris(TextEditor *ed){
     bebaskan_row_list(ed->head);
@@ -60,6 +63,8 @@ void bebaskan_semua_baris(TextEditor *ed){
     ed->jumlah_baris = 0;
 }
 
+
+// Maulida Nur Afifah (017)
 void init_editor(TextEditor *ed) {
     ed->head = NULL;
     ed->tail = NULL;
@@ -78,6 +83,8 @@ void init_editor(TextEditor *ed) {
     ed->keyword_terakhir[0] = '\0';
 }
 
+
+// Maulida Nur Afifah (017)
 // cari node di posisi tertentu dengan traversal 2 arah
 RowNode *cari_node(const TextEditor *ed, int posisi) {
     RowNode *node;
@@ -103,12 +110,16 @@ RowNode *cari_node(const TextEditor *ed, int posisi) {
     return node;
 }
 
+
+//Maulida Nur Afifah (017)
 void reset_hasil_cari(TextEditor *ed){
     ed->jumlah_hasil = 0;
     ed->index_cari = 0;
     ed->keyword_terakhir[0] = '\0';
 }
 
+
+//Maulida Nur Afifah (017)
 int insert_baris(TextEditor *ed, int posisi, const char *isi) {
     RowNode *baris;
     RowNode *target;
@@ -153,32 +164,61 @@ int insert_baris(TextEditor *ed, int posisi, const char *isi) {
     return 0;
 }
 
-// int delete_baris(TextEditor *ed, int posisi) {
-//     int i;
-//     if (ed->jumlah_baris == 0) {
-//         printf("Dokumen sudah kosong.\n");
-//         return -1;
-//     }
-//     if (posisi < 0 || posisi >= ed->jumlah_baris) {
-//         printf("Nomor baris tidak valid: %d\n", posisi + 1);
-//         return -1;
-//     }
 
-//     for (i = posisi; i < ed->jumlah_baris - 1; i++)
-//     memcpy(ed->buffer[i], ed->buffer[i + 1], MAX_KOLOM);
-    
-//     ed->buffer[ed->jumlah_baris - 1][0] = '\0';
-//     ed->jumlah_baris--;
-//     ed->is_modified = 1;
-
-//     if (ed->kursor_baris >= ed->jumlah_baris && ed->kursor_baris > 0)
-//     ed->kursor_baris = ed->jumlah_baris - 1;
-//     ed->kursor_kolom = 0;
-
-//     reset_hasil_cari(ed);
-//     return 0;
-// }
+// Ikhwan Syahid Azizy (013)
+int delete_baris(TextEditor *ed, int posisi) {
+    RowNode *target;
+    RowNode *prev;
+    RowNode *next;
+ 
+    if (ed->jumlah_baris == 0) {
+        printf("Dokumen sudah kosong.\n");
+        return -1;
+    }
+    if (posisi < 0 || posisi >= ed->jumlah_baris) {
+        printf("Nomor baris tidak valid: %d\n", posisi + 1);
+        return -1;
+    }
+ 
+    target = cari_node(ed, posisi);
+    prev = target->prev_row;
+    next = target->next_row;
+ 
+    /* sambungkan tetangga kiri-kanan */
+    if (prev != NULL) prev->next_row = next;
+    else ed->head = next;
+ 
+    if (next != NULL) next->prev_row = prev;
+    else ed->tail = prev;
+ 
+    /* bebaskan node target */
+    free(target->isi);
+    free(target);
+ 
+    ed->jumlah_baris--;
+    ed->is_modified = 1;
+ 
+    /* posisikan ulang kursor */
+    if (ed->jumlah_baris == 0) {
+        ed->kursor_ptr = NULL;
+        ed->kursor_baris = 0;
+    } else if (posisi >= ed->jumlah_baris) {
+        /* baris yang dihapus adalah baris terakhir, kursor mundur */
+        ed->kursor_baris = ed->jumlah_baris - 1;
+        ed->kursor_ptr = ed->tail;
+    } else {
+        /* kursor tetap di posisi, tapi node-nya sekarang adalah 'next' */
+        ed->kursor_baris = posisi;
+        ed->kursor_ptr = next;
+    }
+    ed->kursor_kolom = 0;
+ 
+    reset_hasil_cari(ed);
+    return 0;
+}
    
+
+//Maulida Nur Afifah (017)
 int find_teks(TextEditor *ed, const char *keyword) {
     RowNode *node;
     int nomor = 0;
@@ -226,6 +266,8 @@ int find_teks(TextEditor *ed, const char *keyword) {
     return ed->jumlah_hasil;
 }
  
+
+//Maulida Nur Afifah (017)
 void find_next(TextEditor *ed) {
     HasilCari *h;
  
@@ -247,133 +289,136 @@ void find_next(TextEditor *ed) {
            h->nomor_baris + 1, h->kolom + 1);
 }
 
-// int replace_teks(TextEditor *ed, const char *cari, const char *ganti) {
-//     int used, i, count = 0;
-//     size_t len_cari, len_ganti;
-//     char *src, *pos;
-//     char tmp_baris[MAX_KOLOM]; //buffer sementara di stack
-//     size_t prefix;
 
-//     // memastikan kata yang dicari tidak null
-//     if (cari == NULL || strlen(cari) == 0) return -1;
-//     // jika pengganti null, anggap sebagai string kosong (menghapus kata)
-//     if (ganti == NULL) ganti = "";
-
-//     // menghitung panjang string yg dicari & string pengganti
-//     len_cari = strlen(cari);
-//     len_ganti = strlen(ganti);
-
-//     for (i = 0; i < ed->jumlah_baris; i++) {
-//         if (ed->buffer[i][0] == '\0') continue;
-//         if (strstr(ed->buffer[i], cari) == NULL) continue;
-
-//         // bsngun baris hasil di tmp_baris
-//         tmp_baris[0] = '\0';
-//         used = 0;
-//         src = ed->buffer[i]; // menunjuk ke posisi baca saat ini di baris asli
-//         // cari dan ganti semua kemunculan berikutnya
-//         while ((pos = strstr(src, cari)) != NULL) {
-//             prefix = (size_t)(pos - src); // menghitungg pjg teks sblm kemunculan kata yg ingin diganti
-
-//             // cek apa hasil masih muat di MAX_KOLOM
-//            if (used + (int)prefix + (int)len_ganti >= MAX_KOLOM - 1) {
-//                 // hasil akan melebihi kapasitas baris
-//                 printf("Peringatan: hasil replace di baris %d melebihi %d karakter, dipotong.\n",
-//                     i + 1, MAX_KOLOM - 1);
-//                 break; 
-//         	}
-        	
-//         	// salin teks sebelum kemunculan keyword
-//             memcpy(tmp_baris + used, src, prefix);
-//             used += (int)prefix;
+// Ikhwan Syahid Azizy (013)
+int replace_teks(TextEditor *ed, const char *cari, const char *ganti) {
+    RowNode *node;
+    int count = 0;
+    size_t len_cari, len_ganti, len_lama, len_baru, used, prefix;
+    char *src, *pos, *baris_baru;
  
-//             // salin teks pengganti 
-//             memcpy(tmp_baris + used, ganti, len_ganti);
-//             used += (int)len_ganti;
+    if (cari == NULL || strlen(cari) == 0) return -1;
+    if (ganti == NULL) ganti = "";
  
-//             src = pos + len_cari; // geser posisi baca melewati kemunculan
-//             count++;
-// 		}
-        	
-//         // menyalin sisa teks di baris tsb setelah kemunculan terakhir kata yg dicari
-//         prefix = strlen(src);
-//         if (used + (int)prefix < MAX_KOLOM - 1) {
-//             memcpy(tmp_baris + used, src, prefix + 1); /* +1 untuk null terminator */
-//             used += (int)prefix;
-//         } else {
-//             // potong jika tdk muat
-//             memcpy(tmp_baris + used, src, MAX_KOLOM - 1 - used);
-//             tmp_baris[MAX_KOLOM - 1] = '\0';
-//         }
+    len_cari = strlen(cari);
+    len_ganti = strlen(ganti);
  
-//         // salin hasil kembali ke buffer[i]
-//         strncpy(ed->buffer[i], tmp_baris, MAX_KOLOM - 1);
-//         ed->buffer[i][MAX_KOLOM - 1] = '\0';
-//     }
+    for (node = ed->head; node != NULL; node = node->next_row) {
+        if (node->isi == NULL || node->isi[0] == '\0') continue;
+        if (strstr(node->isi, cari) == NULL) continue;
+ 
+        /* hitung dulu berapa kemunculan di baris ini */
+        int n_match = 0;
+        src = node->isi;
+        while ((pos = strstr(src, cari)) != NULL) {
+            n_match++;
+            src = pos + len_cari;
+        }
+ 
+        if (n_match == 0) continue;
+ 
+        /* alokasi baris baru: panjang lama - (n_match * len_cari) + (n_match * len_ganti) + 1 */
+        len_lama = strlen(node->isi);
+        len_baru = len_lama - (n_match * len_cari) + (n_match * len_ganti);
+        baris_baru = (char *)malloc(len_baru + 1);
+        if (baris_baru == NULL) {
+            fprintf(stderr, "Error: gagal alokasi baris hasil replace.\n");
+            continue;
+        }
+ 
+        /* bangun baris baru dengan substitusi */
+        used = 0;
+        src = node->isi;
+        while ((pos = strstr(src, cari)) != NULL) {
+            prefix = (size_t)(pos - src);
+            memcpy(baris_baru + used, src, prefix);
+            used += prefix;
+            memcpy(baris_baru + used, ganti, len_ganti);
+            used += len_ganti;
+            src = pos + len_cari;
+            count++;
+        }
+        /* salin sisa setelah kemunculan terakhir */
+        prefix = strlen(src);
+        memcpy(baris_baru + used, src, prefix);
+        used += prefix;
+        baris_baru[used] = '\0';
+ 
+        /* ganti isi baris dengan yang baru */
+        free(node->isi);
+        node->isi = baris_baru;
+    }
+ 
+    if (count > 0) {
+        ed->is_modified = 1;
+        printf("Berhasil mengganti %d kemunculan \"%s\" menjadi \"%s\".\n",
+               count, cari, ganti);
+        reset_hasil_cari(ed);
+    } else {
+        printf("Teks \"%s\" tidak ditemukan.\n", cari);
+    }
+ 
+    return count;
+}
 
-//     if (count > 0) {
-//         ed->is_modified = 1;
-//         printf("Berhasil mengganti %d kemunculan \"%s\" menjadi \"%s\".\n", count, cari, ganti);
-//         reset_hasil_cari(ed);
-//     } else {
-//         printf("Teks \"%s\" tidak ditemukan.\n", cari);
-//     }
 
-//     return count;
-// }
+// Ikhwan Syahid Azizy (013)
+void word_count(const TextEditor *ed) {
+    RowNode *node;
+    int total_kata = 0, total_char = 0, dalam_kata = 0;
+    int j;
+    unsigned char c;
+ 
+    for (node = ed->head; node != NULL; node = node->next_row) {
+        if (node->isi == NULL || node->isi[0] == '\0') continue;
+ 
+        j = 0;
+        while ((c = (unsigned char)node->isi[j]) != '\0') {
+            total_char++;
+            if (c == ' ' || c == '\t') {
+                if (dalam_kata) {
+                    total_kata++;
+                    dalam_kata = 0;
+                }
+            } else {
+                dalam_kata = 1;
+            }
+            j++;
+        }
+        if (dalam_kata) {
+            total_kata++;
+            dalam_kata = 0;
+        }
+    }
+ 
+    printf("Statistik dokumen:\n");
+    printf("  Jumlah baris    : %d\n", ed->jumlah_baris);
+    printf("  Jumlah kata     : %d\n", total_kata);
+    printf("  Jumlah karakter : %d\n", total_char);
+}
 
-// void word_count(const TextEditor *ed) {
-//     int i, j;
-//     int total_kata = 0, total_char = 0, dalam_kata = 0;
-//     unsigned char c;
 
-//     for (i = 0; i < ed->jumlah_baris; i++) {
-//         if (ed->buffer[i][0] == '\0') continue;
-//         j = 0;
-//         // membaca karakter satu per satu hingga bertemu null terminator
-//         while ((c = (unsigned char)ed->buffer[i][j]) != '\0') {
-//             total_char++; // menghitung setiap karakter termasuk spasi
-//             // cek apakah karakter saat ini adalah pemisah (spasi atau tab)
-//             if (c == ' ' || c == '\t') {
-//                 // spasi ini menandakan akhir dari satu kata
-//                 if (dalam_kata) { 
-//                     total_kata++; 
-//                     dalam_kata = 0; // reset status karena sekarang di luar kata
-//                 } 
-//             } else { // jika karakter bukan spasi/tab, berarti skrg berada di dalam kata
-//                 dalam_kata = 1;
-//             }
-//             j++;
-//         }
-//         // penanganan akhir baris, baris berakhir dan masih dalam status di dalam kata maka dihitung sbg 1 kata terakhir di bais tsb
-//         if (dalam_kata) { 
-//             total_kata++; 
-//             dalam_kata = 0; 
-//         }
-//     }
-
-//     printf("Statistik dokumen:\n");
-//     printf("  Jumlah baris    : %d\n", ed->jumlah_baris);
-//     printf("  Jumlah kata     : %d\n", total_kata);
-//     printf("  Jumlah karakter : %d\n", total_char);
-// }
-
-// int go_to_line(TextEditor *ed, int nomor) {
-//     int idx = nomor - 1; // konversi nomor baris dari sisi user
-//     // jika kosong, tidak bisa navigasi
-//     if (ed->jumlah_baris == 0) { 
-//         printf("Dokumen kosong.\n"); 
-//         getchar();
-//         return -1; 
-//     }
-//     // validasi nomor baris
-//     if (idx < 0 || idx >= ed->jumlah_baris) {                              
-//         printf("Nomor baris %d tidak valid (1-%d).\n", nomor, ed->jumlah_baris);
-//         getchar();
-//         return -1;
-//     }
-//     // update posisi kursor
-//     ed->kursor_baris = idx; // pindahkan kursor ke baris yang diminta
-//     ed->kursor_kolom = 0;   // reset kolom ke awal baris 
-//     return 0;      
-// }
+// Ikhwan Syahid Azizy (013)
+int go_to_line(TextEditor *ed, int nomor) {
+    int idx = nomor - 1;
+    RowNode *node;
+ 
+    if (ed->jumlah_baris == 0) {
+        printf("Dokumen kosong.\n");
+        getchar();
+        return -1;
+    }
+    if (idx < 0 || idx >= ed->jumlah_baris) {
+        printf("Nomor baris %d tidak valid (1-%d).\n", nomor, ed->jumlah_baris);
+        getchar();
+        return -1;
+    }
+ 
+    node = cari_node(ed, idx);
+    if (node == NULL) return -1;
+ 
+    ed->kursor_ptr = node;
+    ed->kursor_baris = idx;
+    ed->kursor_kolom = 0;
+    return 0;
+}
